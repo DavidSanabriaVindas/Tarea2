4fe2a9ebdb7d85e51269a6e4ef118b800cbc950a
DavidSanabriaVindas <dsanabria30061@ufide.ac.cr>
2026-02-12 01:01:19 -0600

    .

---
+++ VideoGameHub/VideoGameHub/Models/Desarrollador.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace VideoGameHub.Models
+{
+    public class Desarrollador
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Nombre { get; set; }
+
+        public string Pais { get; set; }
+
+        public List<Videojuego> Videojuegos { get; set; }
+    }
+}
